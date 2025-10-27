dac522d17d290e9f1a7ddaf9ae65cba95b621a88
abhishekbisht1 <abhishek.bisht1@gmal.com>
2025-10-27 20:30:28 +0530

    commit

--- src/com/leetcode/sorting/noncomparison/LeetCode2RadixSort.java
+++ src/com/leetcode/sorting/noncomparison/LeetCode2RadixSort.java
@@ -5,10 +5,11 @@ public class LeetCode2RadixSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
+
 		radixSort(arr);
-		
+
 		System.out.println("Radix Sort : ");
-		
+
 		for (int num : arr) {
 			System.out.println(num + " ");
 		}
@@ -26,19 +27,30 @@ public class LeetCode2RadixSort {
 
 	}
 
-	private static void countingSort(int[] arr, int exp) {
+	private static int[] countingSort(int[] arr, int exp) {
 		// TODO Auto-generated method stub
 
 		int n = arr.length;
 		int[] freqArray = new int[10];
+		int[] result = new int[arr.length];
 
 		for (int i = 0; i < arr.length; i++) {
-			freqArray[arr[i] / exp]++;
+			freqArray[(arr[i] / exp) % 10]++;
 		}
 
 		for (int i = 1; i < 10; i++) {
-			freqArray[i]++;
+			freqArray[i] += freqArray[i - 1];
 		}
+
+		for (int i = n - 1; i >= 0; i--) {
+			int digit = (arr[i] / exp) % 10;
+			result[freqArray[digit] - 1] = arr[i];
+			freqArray[digit]--;
+		}
+
+		System.arraycopy(result, 0, arr, 0, n);
+
+		return arr;
 
 	}
 
@@ -46,7 +58,6 @@ public class LeetCode2RadixSort {
 		// TODO Auto-generated method stub
 
 		int max = Integer.MIN_VALUE;
-
 		for (int i = 0; i < arr.length; i++) {
 			if (max < arr[i])
 				max = arr[i];
