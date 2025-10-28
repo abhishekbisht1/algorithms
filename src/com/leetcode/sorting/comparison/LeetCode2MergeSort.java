e5c554e0673e9d0e99ec60d7f42741180ca2fa86
abhishekbisht1 <abhishek.bisht1@gmal.com>
2025-10-28 12:59:16 +0530

    commit

---
+++ src/com/leetcode/sorting/comparison/LeetCode2MergeSort.java
@@ -0,0 +1,41 @@
+package com.leetcode.sorting.comparison;
+
+public class LeetCode2MergeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = { 38, 27, 43, 3, 9, 82, 10 };
+		
+		System.out.println("Before sorting: ");
+		for (int x : arr)
+			System.out.print(x + " ");
+		System.out.println();
+
+		sort(arr, 0, arr.length - 1);
+
+		System.out.println("After sorting: ");
+		for (int x : arr)
+			System.out.print(x + " ");
+
+	}
+
+	private static void sort(int[] arr, int left, int right) {
+		// TODO Auto-generated method stub
+		
+		if(left < right) {
+			int mid = left + right / 2;
+			sort(arr, left , mid);
+			sort(arr, mid + 1, right);
+			merge(arr, left, mid, right);
+			
+		}
+
+	}
+
+	private static void merge(int[] arr, int left, int mid, int right) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
---
+++ src/com/leetcode/sorting/noncomparison/LeetCode3BucketSort.java
@@ -0,0 +1,49 @@
+package com.leetcode.sorting.noncomparison;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LeetCode3BucketSort {
+	public static void main(String[] args) {
+
+		float[] arr = { 0.78f, 0.17f, 0.39f, 0.26f, 0.72f, 0.94f, 0.21f, 0.12f, 0.23f, 0.68f };
+		bucketSort(arr);
+		System.out.println("Bucket SOrt : ");
+
+		for (float num : arr) {
+			System.out.println(num);
+		}
+	}
+
+	private static float[] bucketSort(float[] arr) {
+		// TODO Auto-generated method stub
+		if (arr == null)
+			return null;
+
+		List<Float>[] buckets = new ArrayList[arr.length];
+
+		for (int i = 0; i < 10; i++) {
+			buckets[i] = new ArrayList<>();
+		}
+
+		for (float num : arr) {
+			int index = (int) ((arr.length - 1) * num);
+			buckets[index].add(num);
+		}
+
+		for (List<Float> bucket : buckets) {
+			Collections.sort(bucket);
+		}
+
+		int index = 0;
+		for (List<Float> bucket : buckets) {
+			for (float num : bucket) {
+				arr[index++] = num;
+			}
+		}
+
+		return arr;
+
+	}
+}
