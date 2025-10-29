15e14b6c291ccaa4421b9d2fd321cc6e6c1b2d11
abhishekbisht1 <abhishek.bisht1@gmal.com>
2025-10-30 00:51:35 +0530

    Commit

--- src/com/leetcode/dynamicProgramming/LeetCode3StockBuySellIII.java
+++ src/com/leetcode/dynamicProgramming/LeetCode3StockBuySellIII.java
@@ -1,6 +1,6 @@
 package com.leetcode.dynamicProgramming;
 
-public class LeetCode3StockBuySellI {
+public class LeetCode3StockBuySellIII {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
