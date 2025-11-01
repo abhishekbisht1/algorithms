1e08331562b4bdc05040ef908a34d91a2c7345bc
abhishekbisht1 <abhishek.bisht1@gmail.com>
2025-11-01 21:33:06 +0530

    Commit

--- src/com/leetcode/dynamicProgramming/LeetCode3StockBuySellIIIA.java
+++ src/com/leetcode/dynamicProgramming/LeetCode3StockBuySellIIIA.java
@@ -4,16 +4,34 @@ public class LeetCode3StockBuySellIIIA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int[] prices = { 3, 3, 5, 0, 0, 3, 1, 4 };
-		System.out.println("Max Profit: " + maxProfit(prices));
-		
+
+		// int[] prices = { 3, 3, 5, 0, 0, 3, 1, 4 };
+
+		int[] prices = { 3, 2, 6, 5, 0, 3 };
+
+		int k = 3;
+
+		System.out.println("Max Profit: " + maxProfit(prices, k));
 
 	}
 
-	private static String maxProfit(int[] prices) {
+	private static int maxProfit(int[] prices, int k) {
 		// TODO Auto-generated method stub
-		return null;
+
+		int len = prices.length;
+		int[][] dp = new int[k + 1][len];
+
+		for (int i = 1; i <= k; i++) {
+			int effectiveBuy = prices[0];
+
+			for (int j = 1; j < len; j++) {
+				dp[i][j] = Math.max(dp[i][j - 1], prices[j] - effectiveBuy);
+				effectiveBuy = Math.min(effectiveBuy, prices[j] - dp[i - 1][j]);
+
+			}
+
+		}
+		return dp[k][len - 1];
 	}
 
 }
