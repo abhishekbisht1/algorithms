9d7f5fd4ceb0783ee89ea20564b16c5d64c6ac32
abhishekbisht1 <abhishek.bisht1@gmail.com>
2025-11-04 00:21:33 +0530

    Commit

---
+++ src/com/leetcode/array/LeetCode8maxLengthSubarray.java
@@ -0,0 +1,40 @@
+package com.leetcode.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode8maxLengthSubarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = { 1, 2, 5, -2, 3, -3, 4, 5 };
+		int k = 3; 
+
+		System.out.println(maxLengthSubArray(nums, k));
+
+	}
+
+	private static int maxLengthSubArray(int[] nums, int k) {
+		// TODO Auto-generated method stub
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		int sum = 0, maxLen = 0;
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			sum += nums[i];
+			if (sum == k)
+				maxLen = i + 1;
+
+			if (map.containsKey(sum - k)) {
+				maxLen = Math.max(maxLen, i - map.get(sum - k));
+			}
+
+			map.putIfAbsent(sum, i);
+		}
+
+		return maxLen;
+	}
+
+}
