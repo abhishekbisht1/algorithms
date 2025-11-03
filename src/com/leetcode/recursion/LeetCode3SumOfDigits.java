636e1413837787c0a2f47496b1ec2e7b81d4bad0
abhishekbisht1 <abhishek.bisht1@gmail.com>
2025-11-04 00:43:59 +0530

    Commit

---
+++ src/com/leetcode/recursion/LeetCode3SumOfDigits.java
@@ -0,0 +1,16 @@
+package com.leetcode.recursion;
+
+public class LeetCode3SumOfDigits {
+
+	public static void main(String[] args) {
+		System.out.println(sumDigits(1234));
+	}
+
+	static int sumDigits(int n) {
+		if (n == 0)
+			return 0;
+		
+		return (n % 10) + sumDigits(n / 10);
+	}
+
+}
