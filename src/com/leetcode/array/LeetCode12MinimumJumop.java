88401825331cda86d879824ddcc36404ba1606fb
abhishekbisht1 <abhishek.bisht1@gmal.com>
2025-11-08 02:16:15 +0530

    Commit

---
+++ src/com/leetcode/array/LeetCode12MinimumJumop.java
@@ -0,0 +1,33 @@
+package com.leetcode.array;
+
+public class LeetCode12MinimumJumop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] nums = { 2, 3, 1, 1, 4 };
+
+		System.out.println(jumpFrog(nums));
+
+	}
+
+	private static boolean jumpFrog(int[] nums) {
+		// TODO Auto-generated method stub
+		int maxReach = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (i > maxReach)
+				return false;
+
+			maxReach = Math.max(maxReach, i + nums[i]);
+
+			if (maxReach > nums.length)
+				return true;
+
+		}
+		return true;
+
+	}
+
+}
