e83d6953b88cfb3707637c56e171ccee298c81c0
abhishekbisht1 <abhishek.bisht1@gmail.com>
2025-11-08 00:03:29 +0530

    Coomit

--- src/com/leetcode/LeetCode3ZigZagConversion.java
+++ src/com/leetcode/LeetCode3ZigZagConversion.java
@@ -4,17 +4,6 @@ public class LeetCode3ZigZagConversion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int[] coins = { 1, 2, 5 };
-		int amount = 11;
-		
-		coinChange(coins, amount);
-
-	}
-
-	private static void coinChange(int[] coins, int amount) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
--- src/com/leetcode/dynamicProgramming/LeetCode5CoinChange.java
+++ src/com/leetcode/dynamicProgramming/LeetCode5CoinChange.java
@@ -1,9 +1,37 @@
 package com.leetcode.dynamicProgramming;
 
+import java.util.Arrays;
+
 public class LeetCode5CoinChange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int[] coins = { 1,2,5 };
+		int amount = 11;
+
+		System.out.println(coinChange(coins, amount));
+
+	}
+
+	private static int coinChange(int[] coins, int amount) {
+		// TODO Auto-generated method stub
+
+		int[] dp = new int[amount + 1];
+
+		Arrays.fill(dp, amount + 1);
+
+		dp[0] = 0;
+
+		for (int i = 1; i <= amount; i++) {
+			for (int coin : coins) {
+				if (coin <= i) {
+					dp[i] =  Math.min(dp[i], dp[i - coin] + 1);
+				}
+			}
+		}
+
+		return dp[amount] > amount ? -1 : dp[amount];
 
 	}
 
