a585842fb8d156b73987154628e15675c0db0744
abhishekbisht1 <abhishek.bisht1@gmail.com>
2025-11-09 14:50:50 +0530

    Commit

--- src/com/leetcode/binaryTree/LeetCode3DiameterOfBinaryTree.java
+++ src/com/leetcode/binaryTree/LeetCode3DiameterOfBinaryTree.java
@@ -1,49 +1,46 @@
 package com.leetcode.binaryTree;
 
 public class LeetCode3DiameterOfBinaryTree {
-	
+
 	private static TreeNode_ root;
 	private static int diameter = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		TreeNode_ node = constructBinaryTree(root);
-		System.out.println(diameterOfTree(node));;
+		System.out.println(diameterOfTree(node));
+		;
 		System.out.println(diameter);
 	}
 
 	private static int diameterOfTree(TreeNode_ node) {
 		// TODO Auto-generated method stub
-		if (node == null) return 0;
-		
+		if (node == null)
+			return 0;
+
 		int left = diameterOfTree(node.left);
 		int right = diameterOfTree(node.right);
 
-		
-		diameter =  Math.max(left + right, diameter);
-		
-		
+		diameter = Math.max(left + right, diameter);
+
 		return 1 + Math.max(left, right);
-		
-		
-		
+
 	}
 
 	private static TreeNode_ constructBinaryTree(TreeNode_ node) {
 		// TODO Auto-generated method stub
-		
+
 		node = new TreeNode_(1);
 		node.left = new TreeNode_(2);
 		node.right = new TreeNode_(3);
 		node.left.left = new TreeNode_(4);
 		node.left.right = new TreeNode_(5);
-		
+
 		return node;
 	}
 
 }
-
 
 class TreeNode_ {
 	TreeNode_ left;
--- src/com/leetcode/binaryTree/LeetCode4HeightBinaryTree.java
+++ src/com/leetcode/binaryTree/LeetCode4HeightBinaryTree.java
@@ -2,43 +2,40 @@ package com.leetcode.binaryTree;
 
 public class LeetCode4HeightBinaryTree {
 
-	
 	private static TreeNode_1 root;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		TreeNode_1 node = constructBinaryTree(root);
 		System.out.println(heightOfTree(node));
 	}
 
 	private static int heightOfTree(TreeNode_1 node) {
 		// TODO Auto-generated method stub
-		if (node == null) return 0;
-		
+		if (node == null)
+			return 0;
+
 		int left = heightOfTree(node.left);
 		int right = heightOfTree(node.right);
 
 		return 1 + Math.max(left, right);
-		
-		
-		
+
 	}
 
 	private static TreeNode_1 constructBinaryTree(TreeNode_1 node) {
 		// TODO Auto-generated method stub
-		
+
 		node = new TreeNode_1(1);
 		node.left = new TreeNode_1(2);
 		node.right = new TreeNode_1(3);
 		node.left.left = new TreeNode_1(4);
 		node.left.right = new TreeNode_1(5);
-		
+
 		return node;
 	}
 
 }
-
 
 class TreeNode_1 {
 	TreeNode_1 left;
@@ -59,4 +56,3 @@ class TreeNode_1 {
 		this.data = data;
 	}
 }
-
