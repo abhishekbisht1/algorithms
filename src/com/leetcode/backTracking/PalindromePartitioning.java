a8ca23bee9f4f121b2d1a4074060c9674d876b7a
abhishekbisht1 <abhishek.bisht1@gmal.com>
2025-11-20 00:26:23 +0530

    Commit

--- src/com/leetcode/backTracking/PalindromePartitioning.java
+++ src/com/leetcode/backTracking/PalindromePartitioning.java
@@ -1,10 +1,50 @@
 package com.leetcode.backTracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PalindromePartitioning {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String str = "aab";
+		partitioningPalindrome(str);
+
+	}
+
+	private static List<List<String>> partitioningPalindrome(String str) {
+		// TODO Auto-generated method stub
+
+		List<List<String>> result = new ArrayList<>();
+		backtrack(str, 0, new ArrayList<>(), result);
+		return result;
+
+	}
+
+	private static void backtrack(String str, int start, ArrayList path, List<List<String>> result) {
+		// TODO Auto-generated method stub
+
+		if(start == ) {
+			
+		}
+		
+		for (int end = start; end < str.length(); end++) {
+			if (isPalindrome(str, start, end)) {
+				path.add(str.substring(start, end + 1));
+				backtrack(str, end + 1, path, result);
+				path.remove();
+			}
+		}
+	}
+
+	private static boolean isPalindrome(String str, int left, int right) {
+		// TODO Auto-generated method stub
+		while (left < right) {
+			if (str.charAt(left++) != str.charAt(right--))
+				return false;
+		}
+		return false;
 	}
 
 }
