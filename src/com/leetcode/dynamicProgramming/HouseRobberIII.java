5671bcc8bb606a0314e639c41c219f1c4eee7748
abhishekbisht1 <abhishek.bisht1@gmal.com>
2025-11-29 10:33:23 +0530

    Commit

--- src/com/leetcode/dynamicProgramming/HouseRobberIII.java
+++ src/com/leetcode/dynamicProgramming/HouseRobberIII.java
@@ -1,7 +1,42 @@
 package com.leetcode.dynamicProgramming;
 
 public class HouseRobberIII {
+
+	private static TreeNode root = new TreeNode();
+
 	public static void main(String[] args) {
+		root = createTree(root);
 		
+	}
+
+	private static TreeNode createTree(TreeNode root2) {
+		// TODO Auto-generated method stub
+		
+		root = new TreeNode(3);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.right = new TreeNode(3);
+		root.right.right = new TreeNode(1);
+		return root;
+		
+	}
+}
+
+class TreeNode {
+	TreeNode left;
+	TreeNode right;
+	int val;
+
+	public TreeNode() {
+	}
+
+	public TreeNode(int val) {
+		this.val = val;
+	}
+
+	public TreeNode(TreeNode left, TreeNode right, int val) {
+		this.left = left;
+		this.right = right;
+		this.val = val;
 	}
 }
