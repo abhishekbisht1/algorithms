61e2c68de90c4001d81fe0ce1405de4064caed0a
abhishekbisht1 <abhishek.bisht1@gmail.com>
2025-12-01 23:42:49 +0530

    Commit

---
+++ src/com/leetcode/twoPointers/CircularArrayLoop.java
@@ -0,0 +1,17 @@
+package com.leetcode.twoPointers;
+
+public class CircularArrayLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int [] nums = {2, -1, 1, 2, 2};
+         
+         circularArrayLoop(nums);
+	}
+
+	private static void circularArrayLoop(int[] nums) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
